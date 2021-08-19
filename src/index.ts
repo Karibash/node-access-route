a18f5b242f5b74ca4ef5afe8a42da9e7c7b3e250
Karibash <y.karibayashi@gmail.com>
2021-08-19 22:37:33 +0900

    Add a function to extract the IP address from IncomingMessage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log('hello world');
+import { IncomingMessage } from 'http';
+
+const parseHeader = (request: IncomingMessage): string[] => {
+  if (typeof request.headers.forwarded === 'string') {
+    return request.headers.forwarded
+      .split(';')
+      .filter(value => value.match(/^for=/))
+      .map(value => value.replace('for=', '').trim());
+  }
+
+  if (typeof request.headers['x-forwarded-for'] === 'string') {
+    return request.headers['x-forwarded-for'].split(',').map(value => value.trim());
+  }
+
+  if (typeof request.headers['forwarded-for'] === 'string') {
+    return request.headers['forwarded-for'].split(',').map(value => value.trim());
+  }
+
+  if (typeof request.headers['x-real-ip'] === 'string') {
+    return request.headers['x-real-ip'].split(',').map(value => value.trim());
+  }
+
+  return [];
+};
+
+const remoteAddress = (request: IncomingMessage): string => {
+  if (!request.socket.remoteAddress) {
+    return '127.0.0.1';
+  }
+  return request.socket.remoteAddress;
+};
+
+export default (request: IncomingMessage): string[] => {
+  return [
+    ...parseHeader(request),
+    remoteAddress(request),
+  ].map(ipAddress => {
+    if (ipAddress.substr(0, 7) === '::ffff:') return ipAddress.substr(7);
+    return ipAddress;
+  });
+};
